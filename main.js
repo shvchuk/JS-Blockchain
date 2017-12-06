82f3bc91c7af6c8f2f1f5f51f4e51892bed6de05
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2017-12-06 15:40:37 +0100

    mineBlock function & difficulty

--- main.js
+++ main.js
@@ -7,17 +7,28 @@ class Block{
     this.data = data;
     this.previousHash = previousHash;
     this.hash = this.calculateHash();
+    this.nonce = 0;
   }
 
   calculateHash(){
     return SHA256(this.index + this.previousHash +
-        this.timestamp + JSON.stringify(this.data)).toString();
+        this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+  }
+
+  mineBlock(difficulty){
+    while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+
+    console.log("Block mined: " + this.hash);
   }
 }
 
 class Blockchain{
   constructor(){
     this.chain = [this.createGenesisBlock()];
+    this.difficulty = 2;
   }
 
   createGenesisBlock(){
@@ -30,7 +41,7 @@ class Blockchain{
 
   addBlock(newBlock){
     newBlock.previousHash = this.getLatestBlock().hash;
-    newBlock.hash = newBlock.calculateHash();
+    newBlock.mineBlock(this.difficulty);
     this.chain.push(newBlock);
   }
 
@@ -39,7 +50,16 @@ class Blockchain{
       const currentBlock = this.chain[i];
       const previousBlock = this.chain[i-1];
 
+      if(currentBlock.hash !== currentBlock.calculateHash()){
+        return false;
+      }
+
+      if(currentBlock.previousHash !== previousBlock.hash){
+        return false;
+      }
     }
+
+    return true;
   }
 }
 
@@ -47,4 +67,11 @@ let myCoin = new Blockchain();
 myCoin.addBlock(new Block(1, "06/12/2017", { amount: 4}));
 myCoin.addBlock(new Block(2, "07/12/2017", { amount: 10}));
 
-console.log(JSON.stringify(myCoin, null, 4));
+console.log('Is blockchain valid? ' + myCoin.isChainValid());
+
+myCoin.chain[1].data = {amount: 100};
+myCoin.chain[1].hash = myCoin.chain[1].calculateHash();
+
+console.log('Is blockchain valid? ' + myCoin.isChainValid());
+
+//console.log(JSON.stringify(myCoin, null, 4));
